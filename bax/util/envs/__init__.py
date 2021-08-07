5954b431ce35accd4359f04abe99589ebb4f697d
Viraj Mehta <thebigmehtaphor@gmail.com>
2021-08-06 22:05:24 -0500

    added the pilco cartpole

--- bax/util/envs/__init__.py
+++ bax/util/envs/__init__.py
@@ -1,5 +1,6 @@
 from gym.envs.registration import register
 from bax.util.envs.pendulum import PendulumEnv, pendulum_reward
+from bax.util.envs.pilco_cartpole import CartPoleSwingUpEnv, pilco_cartpole_reward
 from bax.util.envs.goddard import GoddardEnv, goddard_reward
 from bax.util.envs.pets_cartpole import PETSCartpoleEnv, cartpole_reward
 
@@ -29,10 +30,15 @@ register(
     id='petscartpole-v0',
     entry_point=PETSCartpoleEnv,
     )
+register(
+    id='pilcocartpole-v0',
+    entry_point=CartPoleSwingUpEnv,
+    )
 reward_functions = {
         'bacpendulum-v0': pendulum_reward,
         'bacpendulum-tight-v0': pendulum_reward,
         'bacpendulum-medium-v0': pendulum_reward,
         'goddard-v0': goddard_reward,
         'petscartpole-v0': cartpole_reward,
+        'pilcocartpole-v0': pilco_cartpole_reward,
         }
--- bax/viz/__init__.py
+++ bax/viz/__init__.py
@@ -1,7 +1,8 @@
-from bax.viz.plot import plot_pendulum, plot_cartpole
+from bax.viz.plot import plot_pendulum, plot_cartpole, plot_pilco_cartpole
 plotters = {
         'bacpendulum-v0': plot_pendulum,
         'bacpendulum-tight-v0': plot_pendulum,
         'bacpendulum-medium-v0': plot_pendulum,
         'petscartpole-v0': plot_cartpole,
+        'pilcocartpole-v0': plot_pilco_cartpole,
         }
