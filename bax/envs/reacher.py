d0bbf9c89f99794a2101b2c9978d10bc749f29bc
Viraj Mehta <thebigmehtaphor@gmail.com>
2021-08-31 22:56:23 -0500

    added start files

---
+++ bax/envs/reacher.py
@@ -0,0 +1,64 @@
+import numpy as np
+from gym import utils
+from gym.envs.mujoco import mujoco_env
+
+
+class ReacherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        utils.EzPickle.__init__(self)
+        self.horizon = 50
+        self.periodic_dimensions = []
+        mujoco_env.MujocoEnv.__init__(self, "%s/assets/reacher.xml", 2)
+
+    def step(self, a):
+        vec = self.get_body_com("fingertip") - self.get_body_com("target")
+        reward_dist = -np.linalg.norm(vec)
+        reward_ctrl = -np.square(a).sum()
+        reward = reward_dist + reward_ctrl
+        self.do_simulation(a, self.frame_skip)
+        ob = self._get_obs()
+        done = False
+        return ob, reward, done, dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl)
+
+    def reset(self, obs=None):
+        old_obs = super().reset()
+        if obs is None:
+            return old_obs
+        qpos = full_obs[:len(self.init_qpos)]
+        qvel = full_obs[len(self.init_qpos):]
+        self.set_state(qpos, qvel)
+        check_obs = self._get_obs()
+        assert np.allclose(check_obs, obs), f"Obs: {obs} not equal to check_obs {check_obs}"
+        return check_obs
+
+    def viewer_setup(self):
+        self.viewer.cam.trackbodyid = 0
+
+    def reset_model(self):
+        qpos = (
+            self.np_random.uniform(low=-0.1, high=0.1, size=self.model.nq)
+            + self.init_qpos
+        )
+        while True:
+            self.goal = self.np_random.uniform(low=-0.2, high=0.2, size=2)
+            if np.linalg.norm(self.goal) < 0.2:
+                break
+        qpos[-2:] = self.goal
+        qvel = self.init_qvel + self.np_random.uniform(
+            low=-0.005, high=0.005, size=self.model.nv
+        )
+        qvel[-2:] = 0
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def _get_obs(self):
+        theta = self.sim.data.qpos.flat[:2]
+        return np.concatenate(
+            [
+                np.cos(theta),
+                np.sin(theta),
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat[:2],
+                self.get_body_com("fingertip") - self.get_body_com("target"),
+            ]
+        )
