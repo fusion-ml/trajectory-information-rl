6689c3c066a5da13f947855d54afb74507f95ff2
willieneis <willie.neiswanger@gmail.com>
2021-09-06 01:09:12 -0700

    examples: add gpflow/ dir with test scripts for GpflowGp

---
+++ examples/gpflow/test_02.py
@@ -0,0 +1,87 @@
+from argparse import Namespace
+
+import numpy as np
+import tensorflow as tf
+
+from bax.models.gpflow_gp import GpflowGp, get_gpflow_hypers_from_data
+from bax.util.domain_util import unif_random_sample_domain
+
+from branin import branin, branin_xy
+
+
+# Set seed
+seed = 11
+np.random.seed(seed)
+tf.random.set_seed(seed)
+
+# Set numpy print options
+np.set_printoptions(suppress=True, precision=4)
+
+# Set function
+func = branin
+n_dimx = 2
+
+# Set domain
+domain = [[-5, 10], [0, 15]]
+
+# Set data for model
+n_init_data = 10
+data = Namespace()
+data.x = unif_random_sample_domain(domain, n=n_init_data)
+data.y = [func(x) for x in data.x]
+
+
+def join_print_columns(col_list):
+    arr_list = [np.array(col).reshape(-1, 1) for col in col_list]
+    all_arr = np.concatenate(arr_list, axis=1)
+    print(all_arr)
+
+
+# Fit hyperparameters
+gp_params = get_gpflow_hypers_from_data(data, print_fit_hypers=True)
+print(f'gp_params = {gp_params}')
+print('\n--------------------\n\n')
+
+# Define two GpflowGp models
+gpfp_params = dict(fixed_noise=False, noise_var_init=0.01, print_fit_hypers=True)
+gpfp = GpflowGp(gpfp_params, data, verbose=True)
+gpfp_fixedsig_params = dict(fixed_noise=True, print_fit_hypers=True)
+gpfp_fixedsig = GpflowGp(gpfp_fixedsig_params, data, verbose=True)
+
+# Fit both models
+gpfp.fit_hypers()
+gpfp_fixedsig.fit_hypers()
+
+# Extract GPflow models
+model = gpfp.get_gpflow_model()
+model_fixedsig = gpfp_fixedsig.get_gpflow_model()
+
+# Test predictions on gpflow models
+n_test_data = 50
+x_test = unif_random_sample_domain(domain, n=n_test_data)
+y_test = func(x_test)
+
+x_test_gpf = np.array(x_test).reshape(-1, n_dimx)
+y_pred_mean_fixedsig, y_pred_var_fixedsig = model_fixedsig.predict_f(x_test_gpf)
+y_pred_mean, y_pred_var = model.predict_f(x_test_gpf)
+
+col_names = ('y_test', 'y_pred_fixedsig', 'y_pred', '2*std_fixedsig', '2*std')
+print(f'Columns: {col_names}')
+join_print_columns(
+    [
+        y_test,
+        y_pred_mean_fixedsig,
+        y_pred_mean,
+        2*np.sqrt(y_pred_var_fixedsig),
+        2*np.sqrt(y_pred_var),
+    ]
+)
+
+error_model = np.abs(y_test.reshape(-1) -  y_pred_mean.numpy().reshape(-1))
+error_model_fixedsig = np.abs(y_test.reshape(-1) -  y_pred_mean_fixedsig.numpy().reshape(-1))
+
+print(f'mean(error_model) = {np.mean(error_model)}')
+print(f'mean(error_model_fixedsig) = {np.mean(error_model_fixedsig)}')
+
+
+#breakpoint()
