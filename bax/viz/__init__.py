dc7d73a3caa18ca0bdfad336db1f88644bfc4dad
Viraj Mehta <virajm@cs.cmu.edu>
2021-09-08 09:29:31 -0400

    all the cnages required to get trig to work

--- bax/viz/__init__.py
+++ bax/viz/__init__.py
@@ -1,10 +1,13 @@
-from bax.viz.plot import plot_pendulum, plot_cartpole, plot_pilco_cartpole, plot_acrobot, noop
+from bax.viz.plot import plot_pendulum, plot_cartpole, plot_pilco_cartpole, plot_acrobot, noop, plot_trig_pendulum
+from bax.viz.plot import plot_trig_pilco_cartpole
 plotters = {
         'bacpendulum-v0': plot_pendulum,
         'bacpendulum-tight-v0': plot_pendulum,
+        'bacpendulum-trig-v0': plot_trig_pendulum,
         'bacpendulum-medium-v0': plot_pendulum,
         'petscartpole-v0': plot_cartpole,
         'pilcocartpole-v0': plot_pilco_cartpole,
+        'pilcocartpole-trig-v0': plot_trig_pilco_cartpole,
         'bacrobot-v0': plot_acrobot,
         'bacswimmer-v0': noop,
         'bacreacher-v0': noop,
