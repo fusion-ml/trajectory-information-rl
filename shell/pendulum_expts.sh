a529f22a4237f70e4fdd7e6bc5e50b249bec2f32
Viraj Mehta <virajm@cs.cmu.edu>
2022-02-08 11:46:51 -0500

    cfg and script stuff

--- shell/pendulum_expts.sh
+++ shell/pendulum_expts.sh
@@ -1,4 +1,5 @@
 python run.py -m name=bac_pendulum alg=bac num_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
-python run.py -m name=mbrl_pendulum alg=mbrl num_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
-python run.py -m name=random_pendulum alg=random num_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
-python run.py -m name=us_pendulum alg=usnum_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
+# python run.py -m name=bac_pendulum alg=bac num_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
+# python run.py -m name=mbrl_pendulum alg=mbrl num_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
+# python run.py -m name=random_pendulum alg=random num_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
+# python run.py -m name=us_pendulum alg=usnum_iters=200 eval_frequency=5 env=pendulum seed="range(5)" hydra/launcher=joblib
