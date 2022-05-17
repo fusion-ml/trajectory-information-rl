f4654dfef4d9636528986bfb55a763c2b15ce4a8
Viraj Mehta <thebigmehtaphor@gmail.com>
2022-05-16 21:51:40 -0400

    wip notebook

--- shell/reacher_expts.sh
+++ shell/reacher_expts.sh
@@ -5,4 +5,4 @@
 # python run.py -m name=us_reacher alg=us num_iters=1500 eval_frequency=50 env=reacher seed="range(5)" hydra/launcher=joblib
 python run.py -m name=sum_barl_reacher alg=sum_barl num_iters=1500 eval_frequency=50 env=reacher seed="range(5)" hydra/launcher=joblib
 python run.py -m name=sus_reacher alg=sus num_iters=1500 eval_frequency=50 env=reacher seed="range(5)" hydra/launcher=joblib
-python run.py -m name=rus_barl_reacher alg=rus num_iters=1500 eval_frequency=50 env=reacher seed="range(5)" hydra/launcher=joblib
+python run.py -m name=rus_reacher alg=rus num_iters=1500 eval_frequency=50 env=reacher seed="range(5)" hydra/launcher=joblib
